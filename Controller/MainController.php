25f5d87a992982f62722eed5e74a2d28dd4da5a9
Philippe Dellaert <philippe@dellaert.org>
2013-09-14 15:23:20 +0200

    breadcrumbs fix

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -7,7 +7,7 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        $this->get("breadcrumbs")
+        $this->get("white_october_breadcrumbs")
             ->add("Dashboard", $this->get("router")->generate("homepage"));
         return $this->render('DellaertWebappDeploymentBundle:Main:index.html.twig');
     }
