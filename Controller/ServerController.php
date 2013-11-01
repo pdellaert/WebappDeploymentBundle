597b3d53ea6690f68891d4956da05c9de3bb72d4
Philippe Dellaert <philippe@dellaert.org>
2013-11-01 15:03:15 +0100

    fix

--- Controller/ServerController.php
+++ Controller/ServerController.php
@@ -137,7 +137,7 @@ class ServerController extends Controller
                 $entity->setEnabled(true);
                 $entity->preInsert();
 
-                $keyDir = $this->container->getParameter('dellaert_webapp_deployment.data_dir').'/'.$this->container->getParameter('dellaert_webapp_deployment.sshkey_subdir').$entity->getHost();
+                $keyDir = $this->container->getParameter('dellaert_webapp_deployment.data_dir').'/'.$this->container->getParameter('dellaert_webapp_deployment.sshkey_subdir').'/'.$entity->getHost();
                 if( $this->createPushSSHKey($entity,$keyDir) ) {
                     $entity->setSshKeyPath($keyDir.'/wdt');
                     $em = $this->getDoctrine()->getEntityManager();
@@ -222,12 +222,11 @@ class ServerController extends Controller
         
         if( !is_dir($keyDir) ) {
             if(!mkdir($keyDir)) {
-                die("unable to create $keyDir");
                 return false;
             }
         }
 
-        exec('ssh-keygen -q -t rsa -b 4096 -N \'\' -f "'.$keyDir.'/wdt');
+        exec('ssh-keygen -q -t rsa -b 4096 -N \'\' -f "'.$keyDir.'/wdt"');
         if( !is_file($keyDir.'/wdt') ) {
             return false;
         }
